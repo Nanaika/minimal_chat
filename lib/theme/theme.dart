3e92f32ac830e51a1404518c03c13682c986da78
Nanaika <zloyx2@mail.ru>
2024-09-29 19:01:02 +0500

    init commit

---
+++ lib/theme/theme.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+final lightTheme = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade300,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade200,
+    tertiary: Colors.white,
+    inversePrimary: Colors.grey.shade900,
+  ),
+);
+
+final darkTheme = ThemeData(
+  colorScheme: ColorScheme.light(
+    surface: Colors.grey.shade900,
+    primary: Colors.grey.shade500,
+    secondary: Colors.grey.shade800,
+    tertiary: Colors.black,
+    inversePrimary: Colors.grey.shade100,
+  ),
+);
+
+void setLightSystemBars() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Color.fromARGB(1, 1, 1, 1),
+      systemNavigationBarDividerColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.dark
+  ));
+}
+
+void setDarkSystemBars() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+      statusBarIconBrightness: Brightness.light,
+      systemNavigationBarColor: Color.fromARGB(1, 1, 1, 1),
+      systemNavigationBarDividerColor: Colors.transparent,
+      systemNavigationBarIconBrightness: Brightness.light
+  ));
+}
